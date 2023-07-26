5093a3f7ba55bfd99a3cad1e0b481a1289195389
Ben Cassell <ben.cassell@databricks.com>
2023-07-26 16:22:54 -0700

    wip

---
+++ dbt/include/databricks/macros/clone.sql
@@ -0,0 +1,9 @@
+{% macro databricks__can_clone_table() %}
+    {{ return(True) }}
+{% endmacro %}
+
+{% macro databricks__create_or_replace_clone(this_relation, defer_relation) %}
+    create or replace
+    table {{ this_relation }}
+    clone {{ defer_relation }}
+{% endmacro %}
